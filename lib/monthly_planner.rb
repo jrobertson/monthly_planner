a0dfad69732d0d84fe6644a04174bad748f96fcb
Your Name <you@example.com>
2016-07-22 17:43:42 +0100

    feature: The monthly-planner.txt file can now be parsed.

--- lib/monthly_planner.rb
+++ lib/monthly_planner.rb
@@ -7,6 +7,10 @@ require 'dynarex'
 require 'fileutils'
 
 
+# The MonthlyPlanner gem can:
+#
+#  * create a new monthly-planner.txt file
+#  * read an existing monthly-planner.txt file
 
 class MonthlyPlanner
   
@@ -17,7 +21,13 @@ class MonthlyPlanner
     @filename, @path = filename, path
     
     fpath = File.join(path, filename)
-    @dx = new_dx    
+    
+    if File.exists?(fpath) then
+
+      @dx = import_to_dx(File.read(fpath))    
+    else
+      @dx = new_dx    
+    end
   end
 
   def dx()
@@ -40,20 +50,47 @@ class MonthlyPlanner
   
   def dx_to_s(dx)
 
-
-    rows = ['']
+    rows = dx.all.map do |x| 
+      %i(date time desc).map{|y| !x[y].empty? ? x[y] + ' ' : '' }.join
+    end
+    
     title = File.basename(@filename)
-    title + "\n" + "=" * title.length + "\n\n%s\n\n" % [rows.join("\n\n")]
+    title + "\n" + "=" * title.length + "\n\n%s\n\n" % [rows.join("\n")]
 
   end  
   
+  def import_to_dx(s)
+
+    regexp = %r{
+
+      (?<day>\d+(?:-|th|[rn]d|st)){0}
+      (?<month>#{Date::ABBR_MONTHNAMES[1..-1].join('|')}){0}
+      (?<time>\d+[ap]m){0}
+
+      ^(?<date>\g<day>\s*\g<month>)\s*\g<time>?\s*(?<event>.*)
+    }x
+
+    dx = new_dx()
+    a = LineTree.new(s).to_a
+
+    # Remove the heading
+    a.shift 2   
+
+    a.each do |line|
+      r = line[0].match(regexp)
+      dx.create({date: r['date'], time: r['time'], desc: r['event']})
+    end
+    
+    return dx
+  end
+  
   def new_dx()
     
-    dx = Dynarex.new 'month[title]/day(date, desc, uid)'
+    dx = Dynarex.new 'month[title]/day(date, time, desc, uid)', 
+                      default_key: :uid
 
     d = DateTime.now.to_date
-    title = %i(itself next_month)\
-        .map {|x| d.method(x).call.strftime("%b")}.join(' - ')
+    title = [d, d.next_month].map {|x| x.strftime("%b")}.join(' - ')
     dx.title = "Monthly Planner (%s)" % title
 
     return dx
--- monthly_planner.gemspec
+++ monthly_planner.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'monthly_planner'
-  s.version = '0.1.0'
+  s.version = '0.2.0'
   s.summary = 'Stores upcoming dates for the month primarily in a plain text file'
   s.authors = ['James Robertson']
   s.files = Dir['lib/monthly_planner.rb']
