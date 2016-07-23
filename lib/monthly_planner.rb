263628ff00f498d9d4ad02d969d5cd305c59a014
Your Name <you@example.com>
2016-07-23 13:14:20 +0100

    fetaure: Monthly planner entries can now be archived.

--- lib/monthly_planner.rb
+++ lib/monthly_planner.rb
@@ -4,13 +4,25 @@
 
 require 'date'
 require 'dynarex'
+require 'chronic'
 require 'fileutils'
+require 'fuzzy_match'
 
 
 # The MonthlyPlanner gem can:
 #
 #  * create a new monthly-planner.txt file
 #  * read an existing monthly-planner.txt file
+#  * archive and synchronise entries from the monthly-planner with the archive
+
+
+class RecordX
+  
+  def date
+    Chronic.parse(@date)
+  end
+  
+end
 
 class MonthlyPlanner
   
@@ -25,6 +37,7 @@ class MonthlyPlanner
     if File.exists?(fpath) then
 
       @dx = import_to_dx(File.read(fpath))    
+      sync_archive(@dx.all)
     else
       @dx = new_dx    
     end
@@ -49,13 +62,9 @@ class MonthlyPlanner
   private
   
   def dx_to_s(dx)
-
-    rows = dx.all.map do |x| 
-      %i(date time desc).map{|y| !x[y].empty? ? x[y] + ' ' : '' }.join
-    end
-    
+   
     title = File.basename(@filename)
-    title + "\n" + "=" * title.length + "\n\n%s\n\n" % [rows.join("\n")]
+    title + "\n" + "=" * title.length + "\n\n%s\n\n" % [dx.to_s(header: false)]
 
   end  
   
@@ -86,14 +95,67 @@ class MonthlyPlanner
   
   def new_dx()
     
-    dx = Dynarex.new 'month[title]/day(date, time, desc, uid)', 
-                      default_key: :uid
+    dx = Dynarex.new 'month[title]/day(date, time, desc)', default_key: :uid
 
     d = DateTime.now.to_date
     title = [d, d.next_month].map {|x| x.strftime("%b")}.join(' - ')
     dx.title = "Monthly Planner (%s)" % title
 
     return dx
+  end
+  
+  
+  def sync_archive(dxrows=@dx.all)  
+    
+    # group by month
+    a = dxrows.group_by {|x| x.date.strftime("%b").downcase}
+
+    # add the file to the archive.
+
+    a.each do |month, days|
+
+      d = days.first.date
+      
+      archive_path = File.join(@path, d.year.to_s, month)
+      FileUtils.mkdir_p archive_path
+      filepath = File.join archive_path, 'mp.xml'
+
+      if File.exists? filepath then
+
+        dx2 = Dynarex.new filepath
+
+        days.each do |day|
+
+          # determine if the entry already exists in the Dynarex doc
+
+          rows = dx2.to_s(header: false).lines.map(&:chomp)
+          fm = FuzzyMatch.new rows
+          found, score = fm.find_with_score %i(date time desc)\
+                                        .map {|y| day[y]}.join ' '      
+          if score < 0.7 then  
+            dx2.create(day)
+          else
+            i = rows.index found        
+            dx2.update(dx2.all[i].id, day)
+          end
+
+        end
+
+
+      else
+
+        # if the index.xml file doesn't exist just save them
+
+        dx2 = Dynarex.new dx.schema
+        dx2.title = "Monthly Planner #{month.capitalize}-#{d.year}"
+        dx2.import days
+
+      end
+
+      dx2.save filepath
+
+    end
+    
   end
 
 end
--- monthly_planner.gemspec
+++ monthly_planner.gemspec
@@ -1,10 +1,12 @@
 Gem::Specification.new do |s|
   s.name = 'monthly_planner'
-  s.version = '0.2.0'
+  s.version = '0.3.0'
   s.summary = 'Stores upcoming dates for the month primarily in a plain text file'
   s.authors = ['James Robertson']
   s.files = Dir['lib/monthly_planner.rb']
-  s.add_runtime_dependency('dynarex', '~> 1.7', '>=1.7.9')
+  s.add_runtime_dependency('dynarex', '~> 1.7', '>=1.7.11')
+  s.add_runtime_dependency('fuzzy_match', '~> 2.1', '>=2.1.0')
+  s.add_runtime_dependency('chronic', '~> 0.10', '>=0.10.2')
   s.signing_key = '../privatekeys/monthly_planner.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
